6e3ad642cf4056c8e46cbfd1fe94594d015eedf7
Vee-Guz <vero.guzman48612@gmail.com>
2023-04-15 17:09:11 -0700

    included first lines.py and some test cases

--- src/api/lines.py
+++ src/api/lines.py
@@ -4,6 +4,34 @@ from src import database as db
 
 router = APIRouter()
 
+@router.get("/lines/{line_id}", tags=["lines"])
+def get_lines(line_id: int):
+    """
+    This endpoint returns a single line by its identifier. For each line it returns:
+    * `line_id`: the internal id of the line.
+    * `movie_title`: The title of the movie the line is from.
+    * `conversation_id`: the conversation id representing the conversation where the line is spoken.
+    * `character`: character name that says the line.
+    * `line_text`: the line text.
+
+    """
+
+    line = db.lines.get(line_id)
+    if line:
+        character = db.characters.get(line.c_id)
+        movie_title = db.movies[line.movie_id].title
+        result = {
+    		"line_id": line_id,
+    		"movie_title": movie_title,
+    		"conversation_id": line.conv_id,
+    		"character": character.name,
+    		"line_text": line.line_text
+    	}
+
+        return result
+
+    raise HTTPException(status_code=404, detail="line not found.")
+
 # API 1
 # One of the calls must reference the characters speaking the line by name.
 # return all lines from a conversation in a movie in the order spoken
---
+++ test/test_lines.py
@@ -0,0 +1,23 @@
+from fastapi.testclient import TestClient
+
+from src.api.server import app
+
+import json
+
+client = TestClient(app)
+
+
+def test_get_lines():
+    response = client.get("/lines/49")
+    assert response.status_code == 200
+
+    with open("test/lines/49.json", encoding="utf-8") as f:
+        assert response.json() == json.load(f)
+
+
+
+
+
+def test_404():
+    response = client.get("/lines/400")
+    assert response.status_code == 404
