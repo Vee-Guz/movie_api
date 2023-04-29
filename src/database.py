448977782251b6192be48faee5b383b2efada994
Vee-Guz <vero.guzman48612@gmail.com>
2023-04-29 12:58:57 -0700

    Made edits to database to refer to tables in supabase

--- src/database.py
+++ src/database.py
@@ -4,9 +4,16 @@ import os
 import io
 from supabase import Client, create_client
 import dotenv
+from sqlalchemy import create_engine
+import os
+import dotenv
+import sqlalchemy
+import dotenv
 
 # DO NOT CHANGE THIS TO BE HARDCODED. ONLY PULL FROM ENVIRONMENT VARIABLES.
 dotenv.load_dotenv()
+
+## SUPABASE SET UP  
 supabase_api_key = os.environ.get("SUPABASE_API_KEY")
 supabase_url = os.environ.get("SUPABASE_URL")
 
@@ -19,130 +26,29 @@ supabase: Client = create_client(supabase_url, supabase_api_key)
 
 sess = supabase.auth.get_session()
 
-
 def try_parse(type, val):
     try:
         return type(val)
     except ValueError:
         return None
 
+## Postgres
 
-with open("movies.csv", mode="r", encoding="utf8") as csv_file:
-    movies = {
-        try_parse(int, row["movie_id"]): Movie(
-            try_parse(int, row["movie_id"]),
-            row["title"] or None,
-            row["year"] or None,
-            try_parse(float, row["imdb_rating"]),
-            try_parse(int, row["imdb_votes"]),
-            row["raw_script_url"] or None,
-        )
-        for row in csv.DictReader(csv_file, skipinitialspace=True)
-    }
+def database_connection_url():
+    dotenv.load_dotenv()
+    DB_USER: str = os.environ.get("POSTGRES_USER")
+    DB_PASSWD = os.environ.get("POSTGRES_PASSWORD")
+    DB_SERVER: str = os.environ.get("POSTGRES_SERVER")
+    DB_PORT: str = os.environ.get("POSTGRES_PORT")
+    DB_NAME: str = os.environ.get("POSTGRES_DB")
+    return f"postgresql://{DB_USER}:{DB_PASSWD}@{DB_SERVER}:{DB_PORT}/{DB_NAME}"
 
-with open("characters.csv", mode="r", encoding="utf8") as csv_file:
-    characters = {}
-    for row in csv.DictReader(csv_file, skipinitialspace=True):
-        char = Character(
-            try_parse(int, row["character_id"]),
-            row["name"] or None,
-            try_parse(int, row["movie_id"]),
-            row["gender"] or None,
-            try_parse(int, row["age"]),
-            0,
-        )
-        characters[char.id] = char
+# Create a new DB engine based on our connection string
+engine = sqlalchemy.create_engine(database_connection_url())
+metadata_obj = sqlalchemy.MetaData()
 
+movies = sqlalchemy.Table("movies", metadata_obj, autoload_with=engine)
+characters = sqlalchemy.Table("characters", metadata_obj, autoload_with=engine)
+lines = sqlalchemy.Table("lines", metadata_obj, autoload_with=engine)
+conversations = sqlalchemy.Table("conversations", metadata_obj, autoload_with=engine)
 
-## Conversation -- read from csv
-conversation_csv = (
-    supabase.storage.from_("movie-api")
-    .download("conversations.csv")
-    .decode("utf-8")
-)
-
-conversations = {}
-conversations_ls = []
-for row in csv.DictReader(io.StringIO(conversation_csv), skipinitialspace=True):
-    conv = Conversation(
-        try_parse(int, row["conversation_id"]),
-        try_parse(int, row["character1_id"]),
-        try_parse(int, row["character2_id"]),
-        try_parse(int, row["movie_id"]),
-        0,
-        []
-    )
-    conversations[conv.id] = conv
-    conversations_ls.append(row)
-
-def upload_new_conversation():
-    output = io.StringIO()
-    csv_writer = csv.DictWriter(
-        output, fieldnames=[
-        "conversation_id", 
-        "character1_id", 
-        "character2_id", 
-        "movie_id"
-        ]
-    )
-    csv_writer.writeheader()
-    csv_writer.writerows(conversations_ls)
-    supabase.storage.from_("movie-api").upload(
-        "conversations.csv",
-        bytes(output.getvalue(), "utf-8"),
-        {"x-upsert": "true"},
-    )
-
-
-
-## Lines -- read from csv
-
-lines_csv = (
-    supabase.storage.from_("movie-api")
-    .download("lines.csv")
-    .decode("utf-8")
-)
-
-
-lines = {}
-lines_ls = []
-for row in csv.DictReader(io.StringIO(lines_csv), skipinitialspace=True):
-    line = Line(
-        try_parse(int, row["line_id"]),
-        try_parse(int, row["character_id"]),
-        try_parse(int, row["movie_id"]),
-        try_parse(int, row["conversation_id"]),
-        try_parse(int, row["line_sort"]),
-        row["line_text"],
-    )
-    lines[line.id] = line
-    c = characters.get(line.c_id)
-    if c:
-        c.num_lines += 1
-
-    conv = conversations.get(line.conv_id)
-    if conv:
-        conv.num_lines += 1
-        conv.line_ids.append(line.id)
-
-    lines_ls.append(row)
-    
-
-def upload_new_line():
-    output = io.StringIO()
-    csv_writer = csv.DictWriter(
-        output, fieldnames=[
-        "line_id", 
-        "character_id", 
-        "movie_id", 
-        "conversation_id", 
-        "line_sort", 
-        "line_text"]
-    )
-    csv_writer.writeheader()
-    csv_writer.writerows(lines_ls)
-    supabase.storage.from_("movie-api").upload(
-        "lines.csv",
-        bytes(output.getvalue(), "utf-8"),
-        {"x-upsert": "true"},
-    )
