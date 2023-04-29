acac43d5e52c82a01f14e09c40f7b000d5956010
Vee-Guz <vero.guzman48612@gmail.com>
2023-04-29 12:59:44 -0700

    movies.py now runs sql queries

--- src/api/movies.py
+++ src/api/movies.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, HTTPException
 from enum import Enum
 from src import database as db
 from fastapi.params import Query
+import sqlalchemy
 
 router = APIRouter()
 
@@ -22,25 +23,31 @@ def get_movie(movie_id: int):
     * `num_lines`: The number of lines the character has in the movie.
 
     """
+    # get movie
+    txt = "SELECT movie_id, title FROM movies WHERE movies.movie_id = :movie_id"
 
-    movie = db.movies.get(movie_id)
-    if movie:
-        top_chars = [
-            {"character_id": c.id, "character": c.name, "num_lines": c.num_lines}
-            for c in db.characters.values()
-            if c.movie_id == movie_id
-        ]
-        top_chars.sort(key=lambda c: c["num_lines"], reverse=True)
+    # top characters 
+    txt_2 = "SELECT c.character_id AS id, c.name AS name, COUNT(*) num_lines FROM characters AS c JOIN lines ON c.character_id = lines.character_id WHERE c.movie_id = :movie_id GROUP BY c.character_id ORDER BY num_lines DESC LIMIT 5"
 
-        result = {
-            "movie_id": movie_id,
-            "title": movie.title,
-            "top_characters": top_chars[0:5],
-        }
-        return result
+    with db.engine.connect() as conn:
+        result = conn.execute(sqlalchemy.text(txt), [{"movie_id": movie_id}])
+        result_2 = conn.execute(sqlalchemy.text(txt_2), [{"movie_id": movie_id}])
 
-    raise HTTPException(status_code=404, detail="movie not found.")
+        movie_res = None
 
+        for row in result:
+            top_chars = [{"character_id": c.id, "character": c.name, "num_lines": c.num_lines} for c in result_2]
+            
+            movie_res = {
+                "movie_id": movie_id, 
+                "title": row.title, 
+                "top_characters": top_chars,
+            }
+
+        if movie_res is None:
+            raise HTTPException(status_code=404, detail="movie not found.")
+
+        return movie_res
 
 class movie_sort_options(str, Enum):
     movie_title = "movie_title"
@@ -78,33 +85,44 @@ def list_movies(
     maximum number of results to return. The `offset` query parameter specifies the
     number of results to skip before returning results.
     """
-    if name:
-
-        def filter_fn(m):
-            return m.title and name.lower() in m.title
-
+    if sort is movie_sort_options.movie_title:
+        order_by = db.movies.c.title
+    elif sort is movie_sort_options.year:
+        order_by = db.movies.c.year
+    elif sort is movie_sort_options.rating:
+        order_by = sqlalchemy.desc(db.movies.c.imdb_rating)
     else:
+        assert False
 
-        def filter_fn(_):
-            return True
-
-    items = list(filter(filter_fn, db.movies.values()))
-    if sort == movie_sort_options.movie_title:
-        items.sort(key=lambda m: m.title)
-    elif sort == movie_sort_options.year:
-        items.sort(key=lambda m: m.year)
-    elif sort == movie_sort_options.rating:
-        items.sort(key=lambda m: m.imdb_rating, reverse=True)
-
-    json = (
-        {
-            "movie_id": m.id,
-            "movie_title": m.title,
-            "year": m.year,
-            "imdb_rating": m.imdb_rating,
-            "imdb_votes": m.imdb_votes,
-        }
-        for m in items[offset : offset + limit]
+    stmt = (
+        sqlalchemy.select(
+            db.movies.c.movie_id,
+            db.movies.c.title,
+            db.movies.c.year,
+            db.movies.c.imdb_rating,
+            db.movies.c.imdb_votes,
+        )
+        .limit(limit)
+        .offset(offset)
+        .order_by(order_by, db.movies.c.movie_id)
     )
+
+    # filter only if name parameter is passed
+    if name != "":
+        stmt = stmt.where(db.movies.c.title.ilike(f"%{name}%"))
+
+    with db.engine.connect() as conn:
+        result = conn.execute(stmt)
+        json = []
+        for row in result:
+            json.append(
+                {
+                    "movie_id": row.movie_id,
+                    "movie_title": row.title,
+                    "year": row.year,
+                    "imdb_rating": row.imdb_rating,
+                    "imdb_votes": row.imdb_votes,
+                }
+            )
 
     return json
