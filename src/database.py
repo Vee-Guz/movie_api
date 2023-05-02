bfdb943b3546e2f2565de29242ee617d565e3cbd
Vee-Guz <vero.guzman48612@gmail.com>
2023-05-02 14:25:02 -0700

    removed csv and supabase requirement

--- src/database.py
+++ src/database.py
@@ -1,7 +1,5 @@
 import csv
-from src.datatypes import Character, Movie, Conversation, Line
 import os
-import io
 from supabase import Client, create_client
 import dotenv
 import sqlalchemy
@@ -9,19 +7,6 @@ import sqlalchemy
 
 # DO NOT CHANGE THIS TO BE HARDCODED. ONLY PULL FROM ENVIRONMENT VARIABLES.
 dotenv.load_dotenv()
-
-## SUPABASE SET UP  
-supabase_api_key = os.environ.get("SUPABASE_API_KEY")
-supabase_url = os.environ.get("SUPABASE_URL")
-
-if supabase_api_key is None or supabase_url is None:
-    raise Exception(
-        "You must set the SUPABASE_API_KEY and SUPABASE_URL environment variables."
-    )
-
-supabase: Client = create_client(supabase_url, supabase_api_key)
-
-sess = supabase.auth.get_session()
 
 def try_parse(type, val):
     try:
